857f6aed3df9cdaaef5af47a8428775a30daaaa9
klopina <klopinamykola@gmail.com>
2020-11-28 15:54:19 +0200

    Рабочий вариант. Валидатор защитал)

--- crossword.java
+++ crossword.java
@@ -1,57 +1,53 @@
-package com.company;
+package com.javarush.task.task20.task2027;
 
-import java.util.*;
-
-public class Main {
-    /*
-1. Дан двумерный массив, который содержит буквы английского алфавита в нижнем регистре.
-2. Метод detectAllWords должен найти все слова из words в массиве crossword.
-3. Элемент(startX, startY) должен соответствовать первой букве слова, элемент(endX, endY) - последней.
-text - это само слово, располагается между начальным и конечным элементами
-4. Все слова есть в массиве.
-5. Слова могут быть расположены горизонтально, вертикально и по диагонали как в нормальном, так и в обратном порядке.
-6. Метод main не участвует в тестировании.
-7. И всякая хрень.
-  */
-
+import java.util.ArrayList;
+import java.util.List;
+/*
+Кроссворд
+*/
+class Solution {
     public static void main(String[] args) {
-        int[][] crossword = new int[][]{
-                {'f', 'd', 'e', 'r', 'l', 'k'},
-                {'u', 's', 'a', 'm', 'e', 'o'},
-                {'l', 'n', 'g', 'r', 'o', 'v'},
-                {'m', 'l', 'p', 'r', 'r', 'h'},
-                {'p', 'o', 'e', 'e', 'j', 'j'}
-        };
+        int[][] crossword = new int[][]{{'f', 'd', 'e', 'r', 'l', 'k'},
+                                        {'u', 's', 'a', 'm', 'e', 'o'},
+                                        {'l', 'n', 'g', 'r', 'o', 'v'},
+                                        {'m', 'l', 'p', 'r', 'r', 'h'},
+                                        {'p', 'o', 'e', 'e', 'j', 'j'}};
 
+//          detectAllWords(crossword, "home", "same");
         for (Word x : detectAllWords(crossword, "home", "same"))
             System.out.println(x);
+        /*
+Ожидаемый результат
+home - (5, 3) - (2, 0)
+same - (1, 1) - (4, 1)
+         */
     }
-
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
         List<Word> allWords = new ArrayList<>();
-        Word word;
-        for (String w : words) {
-            word = new Word(w);
-            //create 8 directions and designate them with coordinates. For example: (-1, 1) - diagonal to the left, (0, -1) - horizontal to the left.
-            int[] dy = new int[]{0, 1, 1, 1, 0, -1, -1, -1},
-                    dx = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
-
-            for (int y = 0; y < crossword.length; y++) {
-                for (int x = 0; x < crossword[y].length; x++) {
-                    if ((char) crossword[y][x] == w.charAt(0)) {
-                        for (int i = 0; i < dx.length; i++) {
+        for (String st : words) {
+            Word word = new Word(st);
+            int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
+            for (int row = 0; row < crossword.length; row++) {
+                for (int col = 0; col < crossword[0].length; col++) {
+                    if (crossword[row][col] == st.charAt(0)) {
+                        int len = st.length();
+                        for (int dir = 0; dir < x.length; dir++) {
                             boolean thisWord = true;
-                            for (int c = 1; c < w.length(); c++) {
-                                //y+c*dy[i] - is a formula for determining direction, where "c" is equal to the number of a letter in a word.
-                                if (y+c*dy[i] < 0 || y+c*dy[i] >= crossword.length || x+c*dx[i] < 0 || x+c*dx[i] >= crossword[y+c*dy[i]].length ||
-                                        (char) crossword[y+c*dy[i]][x+c*dx[i]] != w.charAt(c)) {
+                            int  rd = row + x[dir], cd = col + y[dir];
+                            for (int k = 1; k < len; k++) {
+                                if (rd >= crossword.length || rd < 0 || cd >= crossword[0].length || cd < 0 || crossword[rd][cd] != st.charAt(k)) {
                                     thisWord = false;
                                     break;
                                 }
+                                if (k < len - 1) {
+                                    rd += x[dir];
+                                    cd += y[dir];
+                                }
                             }
                             if (thisWord) {
-                                word.setStartPoint(x, y);
-                                word.setEndPoint(x+dx[i]*(w.length()-1), y+(w.length() - 1)*dy[i]);
+                                word.setStartPoint(col, row);
+                                word.setEndPoint(cd, rd);
                                 allWords.add(word);
                             }
                         }
